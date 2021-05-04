e9a7a6363383d0bb069d997de89be43526165032
WinAuD <mailchris1@gmx.de>
2021-05-04 20:14:58 +0200

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -3,19 +3,27 @@ import "./App.css";
 import Description from './Description.js'
 import Input from './Input.js'
 import Output from './Output.js'
+import { Route, NavLink, HashRouter } from "react-router-dom";
  
 class NameTag extends Component {
  
   render() {
     return (
+    <HashRouter>
       <div>
         <h1>Reader Page</h1>
         <ul>
-          <li ><a href="/">Description</a></li>
+          <li ><a href="/" >Description</a></li>
           <li><a href="/batman">Input</a></li>
           <li><a href="/spiderman">Output</a></li>
         </ul>
+        <div className="myHeroContent">
+    <Route path="/" component={Description} />
+    <Route path="/bat" component={Input} />
+    <Route path="/spid" component={Output} />
+  </div>
       </div>
+     </HashRouter>
     )
   }
 }
